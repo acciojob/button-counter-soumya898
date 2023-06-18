9aec34d0cf7da1f86ce99a5f7d9a8e926f2a10e0
Soumya Ranjan Nayak <soumyaranjannayak0140@gmail.com>
2023-06-18 12:41:49 +0530

    submitting code-Sun Jun 18 2023 12:41:47 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -9,8 +9,11 @@ const App = () => {
   let [count,setCount]=useState(0);
   return (
     <div>
-      <p>Button clicked{count} times </p>
-        { <button onClick={setCount(++count)}> Click Me  </button>}
+       <p>Button clicked {count} times</p>
+        { 
+       
+        <button onClick={setCount(++count)}> Click Me  </button>
+        }
     </div>
   )
 }
